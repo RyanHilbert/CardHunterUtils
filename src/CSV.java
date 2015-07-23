6294939829ea75d90e375c464794c5ee74a120a1
Matt C. Wilson <matthew.c.wilson@gmail.com>
2015-07-23 00:19:00 -0400

    Update CSV.java to handle quoted text in flavor text fields

--- src/CSV.java
+++ src/CSV.java
@@ -1,26 +1,48 @@
+
+import java.util.ArrayList;
+
 public final class CSV{
     private CSV(){};
     public static String[]tokenizeLine(String string){
-        String[]tokens=new String[1+string.length()-string.replace(",","").length()];
-        boolean quoted=false;
-        int tokenStart=0,arrayIndex=0;
-        for(int i=0;i<string.length();++i){
+        ArrayList<String> tokens=new ArrayList<String>(1+string.length()-string.replace(",","").length());
+        boolean inQuotes=false;
+        int tokenStart=0, arrayIndex=0, len=string.length();
+        for(int i=0;i<len;++i){
             final char c=string.charAt(i);
-            if(c==','&&!quoted){
-                tokens[arrayIndex++]=string.substring(tokenStart,i).replace("\"","");
-                tokenStart=i+1;
-            }
-            else if(c=='"'){
-                if(quoted){
-                    if(string.charAt(i+1)=='"')++i;//skip double quotes in quoted sections
-                    else quoted=false;
+            final char n=(i==len-1) ? ' ' : string.charAt(i+1);
+
+            if(inQuotes){ // look ahead for the end of the quoted field we're in.
+                if(c=='"'){
+                    if(n!='"'){ // if there's a quote not immediately followed by a quote, we're leaving the quoted block.
+                        inQuotes=false;
+                        if(n==',') { // we're leaving the token as well, so capture it...
+                            String token=string.substring(tokenStart,i).replace("\"\"","\"");
+                            tokens.add(token);
+                            i++; // ... and advance pointers into the next token...
+                            tokenStart=i+1;
+                            if (string.charAt(tokenStart)=='"') { // ... and, if we're re-entering a (2nd) quoted field, flag it.
+                                i++;
+                                tokenStart++;
+                                inQuotes=true;
+                            }
+                        }
+                    }
+                    else // otherwise, this is a two-quote sequence, which will resolve to a single quote in the output, so skip it.
+                        i++; 
                 }
-                else{
-                    quoted=true;
+            }
+            else if(c==','){ // we're in a standard token, so look for a comma...
+                String token=string.substring(tokenStart,i).replace("\"\"","\""); // ... capture the token...
+                tokens.add(token);
+                tokenStart=i+1;
+                if(n=='"'){ // ... and check for whether we're entering a new, quoted field.  If so, flag it.
+                    i++;
+                    tokenStart++;
+                    inQuotes=true;
                 }
             }
         }
-        tokens[arrayIndex]=string.substring(tokenStart,string.length());
-        return tokens;
+        tokens.add(string.substring(tokenStart,string.length()).replace("\"\"","\"")); // capture whatever's left on the line as the final token
+        return tokens.toArray(new String[tokens.size()]);
     }
 }
