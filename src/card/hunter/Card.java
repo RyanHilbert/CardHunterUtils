bfaddb9fbfff3418a0a89f9e269f50cbf4d3c556
Ryan Hilbert <Ryan.J.Hilbert@gmail.com>
2015-09-30 02:36:51 -0400

    Finished card thumbnails - Titlebars could still use tweaking

--- src/card/hunter/Card.java
+++ src/card/hunter/Card.java
@@ -3,12 +3,13 @@ package card.hunter;
 import card.hunter.io.*;
 import java.util.*;
 import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
+import javafx.geometry.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
 import javafx.scene.layout.*;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 import javafx.scene.text.*;
 
 public final class Card extends Collectible<Card>{
@@ -16,7 +17,7 @@ public final class Card extends Collectible<Card>{
     public final Card_Type type1,type2;
 	public final Attack_Type attackType;
     public final Damage_Type damageType;
-	public final byte damage,range,movement,roll;
+	public final byte damage,range,movement,roll1,roll2;
 	public final String text,flavorText,triggerText1,triggerText2;
     public final Quality quality;
     private Image thumbnail;
@@ -54,8 +55,8 @@ public final class Card extends Collectible<Card>{
 		}
 	}
 	public Card(int id,String name,String shortName,Rarity rarity,Set set,byte level,Image illustration,
-				Card_Type type1,Card_Type type2,Attack_Type attackType,Damage_Type damageType,byte damage,byte roll,
-				byte range,byte move,String text,String flavor,String trigger1,String trigger2,Quality quality){
+				Card_Type type1,Card_Type type2,Attack_Type attackType,Damage_Type damageType,byte damage,byte roll1,
+				byte roll2,byte range,byte move,String text,String flavor,String trigger1,String trigger2,Quality quality){
 		super(id,name,shortName,rarity,set,level,illustration);
 		this.type1=type1;
 		this.type2=type2;
@@ -64,7 +65,8 @@ public final class Card extends Collectible<Card>{
 		this.damage=damage;
 		this.range=range;
 		this.movement=move;
-		this.roll=roll;
+		this.roll1=roll1;
+		this.roll2=roll2;
 		this.text=text;
 		this.flavorText=flavor;
 		this.triggerText1=trigger1;
@@ -82,7 +84,8 @@ public final class Card extends Collectible<Card>{
 		damage=row.getByte("Damage");
 		range=row.getByte("Range");
 		movement=row.getByte("Move Points");
-		roll=row.getByte("Trigger");
+		roll1=row.getByte("Trigger");
+		roll2=row.getByte("Trigger 2");
 		
 		final String[]types=row.getString("Types").split(",");
 		type1=Card_Type.valueOf(types[0]);
@@ -97,6 +100,12 @@ public final class Card extends Collectible<Card>{
 			illustration=Assets.card_illustrations.load(name);
 			thumbnail=Assets.card_thumbnails.load(name);
 		}
+	}
+	public boolean isPlayable(){
+		return!text.isEmpty()||type1==Card_Type.Assist||type1==Card_Type.Attack||type1==Card_Type.Move||type1==Card_Type.Utility;
+	}
+	public boolean isTriggerable(){
+		return!triggerText1.isEmpty();
 	}
 	public Card_Type getType1(){
 		return type1;
@@ -120,7 +129,7 @@ public final class Card extends Collectible<Card>{
 		return movement;
 	}
 	public byte getRoll(){
-		return roll;
+		return roll1>0?roll1:roll2;
 	}
 	public String getText(){
 		return text;
@@ -143,23 +152,40 @@ public final class Card extends Collectible<Card>{
 	@Override public View getView() {
 		return new View();
 	}
+	public class FullView extends ImageView{//placeholder for actual fullsize view class
+		public FullView(){
+			super(illustration);
+			setFitWidth(217);
+			setFitHeight(129);
+		}
+	}
 	public class View extends Group{
 		ContextMenu menu=new ContextMenu(new CustomMenuItem(new FullView()));
 		public View(){
 			ObservableList<Node>children=getChildren();
-			children.add(new Rectangle(74,101,type1.color));
+			Rectangle primary=new Rectangle(74,101,type1.color);
+			primary.setStroke(Color.BLACK);
+			primary.setStrokeType(StrokeType.INSIDE);
+			primary.setStrokeWidth(2);
+			children.add(primary);
 			if(type2!=type1){
-				Rectangle secondary=new Rectangle(37,0,37,101);
+				Rectangle secondary=new Rectangle(36,18,36,81);
 				secondary.setFill(type2.color);
 				children.add(secondary);
 			}
 			Label title=new Label(shortName);
-			title.setTextAlignment(TextAlignment.CENTER);
-			title.setFont(new Font(10));
-			title.setMinSize(74,16);
-			title.setMaxSize(74,16);
+			title.setAlignment(Pos.CENTER);
+			title.setFont(Font.font("Times New Roman",FontWeight.BOLD,11));
+			title.setMinSize(74,18);
+			title.setMaxSize(74,18);
 			title.setBackground(new Background(new BackgroundFill(quality.color(),CornerRadii.EMPTY,Insets.EMPTY)));
 			children.add(title);
+			
+			Rectangle background=new Rectangle(5,21,64,39);
+			background.setFill(type1.color);
+			background.setStroke(Color.BLACK);
+			background.setStrokeWidth(2);
+			children.add(background);
 			
 			ImageView view=new ImageView(thumbnail);
 			view.setFitWidth(62);
@@ -168,13 +194,63 @@ public final class Card extends Collectible<Card>{
 			view.setY(22);
 			children.add(view);
 			setOnMouseClicked(event->menu.show(this,event.getScreenX(),event.getScreenY()));
-		}
-	}
-	public class FullView extends ImageView{//placeholder for actual fullsize view class
-		public FullView(){
-			super(illustration);
-			setFitWidth(217);
-			setFitHeight(129);
+			
+			boolean row2=false;
+			final byte x1=4,x2=38,y1=63,y2=81;
+			if(isPlayable()){
+				view=new ImageView(damage<0?movement<0?Assets.PLAY:Assets.get("Move"+movement):Assets.get("Damage"+damage));
+				view.setLayoutX(x1);
+				view.setLayoutY(y1);
+				children.add(view);
+				view=new ImageView(range<0?Assets.PLAY:Assets.get("Range"+range));
+				view.setLayoutX(x2);
+				view.setLayoutY(y1);
+				children.add(view);
+				row2=true;
+			}
+			if(!triggerText1.isEmpty()){
+				view=new ImageView(roll1<=0?Assets.TRIGGER:Assets.get("Roll"+roll1));
+				view.setLayoutX(x1);
+				view.setLayoutY(row2?y2:y1);
+				children.add(view);
+				final Image image;
+				switch(type1){
+					case Armor:image=Assets.PLATE;break;
+					case Block:image=Assets.SHIELD;break;
+					case Boost:image=Assets.STAR;break;
+					case Handicap:image=Assets.BOLT;break;
+					default:switch(type2){
+						case Armor:image=Assets.PLATE;break;
+						case Block:image=Assets.SHIELD;break;
+						case Handicap:image=Assets.BOLT;break;
+						default:image=Assets.STAR;
+					}
+				}
+				view=new ImageView(image);
+				view.setLayoutX(x2);
+				view.setLayoutY(row2?y2:y1);
+				children.add(view);
+				row2=true;
+				if(!triggerText2.isEmpty()){
+					view=new ImageView(roll2<=0?Assets.TRIGGER:Assets.get("Roll"+roll2));
+					view.setLayoutX(x1);
+					view.setLayoutY(y2);
+					children.add(view);
+					view=new ImageView(Assets.STAR);
+					view.setLayoutX(x2);
+					view.setLayoutY(y2);
+				}
+			}
+			if(!row2){
+				view=new ImageView(Assets.PLAY);
+				view.setLayoutX(x1);
+				view.setLayoutY(y1);
+				children.add(view);
+				view=new ImageView(Assets.PLAY);
+				view.setLayoutX(x2);
+				view.setLayoutY(y1);
+				children.add(view);
+			}
 		}
 	}
 	private void readObject(java.io.ObjectInputStream in)throws java.io.IOException,ClassNotFoundException{
