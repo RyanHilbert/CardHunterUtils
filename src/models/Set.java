e5f24fc2dc164b5ae5696ee5e6579e4edb7550fc
Ryan Hilbert <ryan.j.hilbert@gmail.com>
2019-03-17 16:31:50 -0400

    Added missing enum values from Castle Mitternacht expansion

--- src/models/Set.java
+++ src/models/Set.java
@@ -1,7 +1,7 @@
 package models;
 
 public enum Set{
-    Base,AotA,Cit,AA;
+    Base,AotA,Cit,AA,AI,CM;
     private final static Set[]values=Set.values();
     public static Set value(int i){return values[i];}
 }
